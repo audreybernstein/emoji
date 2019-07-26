b1f5905718e01bafe7817e38678759ce5f52bc99
audreybernstein <42146491+audreybernstein@users.noreply.github.com>
2019-07-26 07:51:58 -0700

    Add files via upload

---
+++ Emojional/Emojional/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Emojional
+//
+//  Created by Apple on 7/25/19.
+//  Copyright © 2019 KWK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func showMessage(sender: UIButton) {
+        let alertController = UIAlertController(title: "Swiftoji", message: "Have you ever thought just maybe you belong with me?" , preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
